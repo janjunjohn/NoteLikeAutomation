631e411ddc22dacc0e1e1595669673a0d0263cef
Jun Kakishima <adventureocean3699@gmail.com>
2023-05-10 11:34:36 +0900

    環境変数の設定

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
 
 import os
+
 MYAPP_USER = os.getenv('MYAPP_USER')
 MYAPP_PASS = os.getenv('MYAPP_PASS')
 DRIVER_PATH = os.getenv('DRIVER_PATH')
@@ -35,6 +39,8 @@ class LikeAutomation:
         self.login_to_note()
 
     def login_to_note(self):
+        option = Options()
+        option.add_argument('--headless')
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
         self.driver.get('https://note.com')
 
@@ -50,7 +56,7 @@ class LikeAutomation:
 
         password_input = self.driver.find_element(By.ID, 'password')
         password_input.send_keys(PASSWORD + Keys.ENTER)
-        time.sleep(10)
+        time.sleep(15)
 
     def search_and_like(self):
         search_box = self.driver.find_element(
@@ -59,6 +65,7 @@ class LikeAutomation:
         )
         search_box.send_keys(self.search_word + Keys.ENTER)
         time.sleep(10)
+        
         post_list = self.driver.find_elements(By.CLASS_NAME, 'm-largeNoteWrapper__card')
         push_like_count = 0
         for post in post_list:
@@ -74,7 +81,62 @@ class LikeAutomation:
                 break
 
     def return_like(self):
-        pass
+        user_icon = self.driver.find_element(
+            By.XPATH, '/html/body/div/div/div/div[1]/header/span/div/nav/div[3]/button'
+        )
+        user_icon.click()
+        time.sleep(2)
+        nav_note = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div/div/div/div[1]/header/span/div/nav/div[3]/div/div/div/section[1]/ul/li[2]/a/div/span',
+        )
+        nav_note.click()
+        time.sleep(10)
+        note_list = self.driver.find_elements(By.CLASS_NAME, 'o-articleList__link')
+        note_list[0].click()
+        time.sleep(7)
+
+        push_like_count = 0
+
+        like_number = self.driver.find_element(
+            By.XPATH,
+            '/html/body/div/div/div/div[1]/div[2]/main/div[1]/article/div[1]/div/div/div[1]/div[1]/div/span/button',
+        )
+        like_number.click()
+        time.sleep(3)
+        modal_user_list = self.driver.find_elements(By.CLASS_NAME, 'm-userList__item')
+        for target_user in modal_user_list:
+            if push_like_count == self.like_count:
+                break
+            try:
+                print(target_user.text)
+                if 'アジアンビジネスネットワーク' in target_user.text:
+                    break
+                target_user_url = target_user.find_element(
+                    By.TAG_NAME, 'a'
+                ).get_attribute('href')
+                self.driver.execute_script(f"window.open('{target_user_url}');")
+                self.driver.switch_to.window(self.driver.window_handles[1])
+                time.sleep(5)
+                post_list = self.driver.find_elements(
+                    By.CLASS_NAME, 'm-largeNoteWrapper__card'
+                )
+                for post in post_list:
+                    try:
+                        like_target = post.find_element(By.CLASS_NAME, 'a-icon--heart')
+                        like_target.click()
+                        push_like_count += 1
+                        print(f'「{post.text}」\n"スキを押しました。"\n')
+                        break
+                    except:
+                        print('すでに"スキ"済み。\n')
+                        continue
+                self.driver.close()
+                self.driver.switch_to.window(self.driver.window_handles[0])
+                time.sleep(4)
+
+            except:
+                continue
 
     def quit_driver(self):
         self.driver.quit()
